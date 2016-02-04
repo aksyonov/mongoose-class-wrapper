2129b9c4abafbd5da2bb82240179cf4b70e63380
Alexander Plesovskikh <me@hgen.ru>
2016-02-04 16:01:18 +0500

    add getters and setters wrap for static methods, remove deprecated decorator support, update babel

--- src/index.js
+++ src/index.js
@@ -6,16 +6,21 @@ function wrap(schema, klass) {
   let prototypeProps = Object.getOwnPropertyNames(proto);
   let instanceProps = prototypeProps.filter(name => name !== 'constructor');
 
+  let processVirtual = (name, method) => {
+    if (typeof method.get == 'function') schema.virtual(name).get(method.get);
+    if (typeof method.set == 'function') schema.virtual(name).set(method.set);
+  };
+
   staticProps.forEach(name => {
     let method = Object.getOwnPropertyDescriptor(klass, name);
     if (typeof method.value == 'function') schema.static(name, method.value);
+    processVirtual(name, method);
   });
 
   instanceProps.forEach(name => {
     let method = Object.getOwnPropertyDescriptor(proto, name);
     if (typeof method.value == 'function') schema.method(name, method.value);
-    if (typeof method.get == 'function') schema.virtual(name).get(method.get);
-    if (typeof method.set == 'function') schema.virtual(name).set(method.set);
+    processVirtual(name, method);
   });
 }
 
