f267bbc09efdbcaeac69a4a7f3cd2b1f14b54f86
Alexander Plesovskikh <me@hgen.ru>
2016-02-04 17:03:05 +0500

    remove old requires

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
 module.exports = require('./lib');
-var decorators = require('./lib/decorators.js');
-module.exports.mongooseModel = decorators.mongooseModel;
-module.exports.plugin = decorators.plugin;
-module.exports.index = decorators.index;
-module.exports.post = decorators.post;
-module.exports.pre = decorators.pre;
